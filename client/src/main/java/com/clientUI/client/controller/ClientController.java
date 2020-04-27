cf40a74dd8a946b412dba99cc60c6668e49b18f1
Mouslim Aitssi <mouslimaitssi@gmail.com>
2020-04-27 23:16:05 +0000

    clientUI works (before feign)

--- client/src/main/java/com/clientUI/client/controller/ClientController.java
+++ client/src/main/java/com/clientUI/client/controller/ClientController.java
@@ -1,14 +1,13 @@
 package com.clientUI.client.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping("/")
 public class ClientController {
+    @RequestMapping("/")
     public String accueil(Model model) {
         return "Accueil";
     }
