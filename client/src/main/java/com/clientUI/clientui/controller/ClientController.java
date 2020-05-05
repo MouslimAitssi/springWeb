86438f2262875c205aaaa7ea887cd37cf7bd2178
Mouslim Aitssi <mouslimaitssi@gmail.com>
2020-05-05 00:14:41 +0000

    css loaded

--- client/src/main/java/com/clientUI/clientui/controller/ClientController.java
+++ client/src/main/java/com/clientUI/clientui/controller/ClientController.java
@@ -5,6 +5,8 @@ import com.clientUI.clientui.proxies.JeuxProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,7 +25,7 @@ public class ClientController {
 
         model.addAttribute("produits", produits);
 
-        return "Accueil";
+        return "index";
     }
 
 }
